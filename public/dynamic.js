87f796aa74f06ce0a7852da81885dedf1169a252
omarcodex <omaralimalik@gmail.com>
2017-07-29 14:52:26 -0700

    Add basic text-font translation functionality via button submit

--- public/dynamic.js
+++ public/dynamic.js
@@ -1,1 +1,15 @@
-console.log('ready');
+// Code for simulating translation into Indus script.
+// By O.A.M. 2017 @ omarcodex
+
+let userInput = document.getElementById('user-typed-input');
+let userOutput = document.getElementById('user-typed-output');
+document.getElementById('user-typed-input-btn').addEventListener('click', translateText);
+
+function translateText(e) {
+  e.preventDefault();
+  console.log('submitted!');
+  console.log(userInput.value);
+  let userTranslatedText = userInput.value;
+  userOutput.classList.add('glyph');
+  userOutput.innerText = userTranslatedText;
+}
