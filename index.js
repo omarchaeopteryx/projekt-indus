7f9feeb56923dd2f7167da03d0a1eaf6f983bb2a
omarcodex <omaralimalik@gmail.com>
2017-07-29 17:18:27 -0700

    Update port listener for dynamic assigning

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res) {
   res.render('public/index.html');
 });
 
-app.listen(3000, function() {
-  console.log('App listening on port 3000!');
+app.listen(process.env.PORT || 5000, function() {
+  console.log('App listening on assigned port!');
 });
